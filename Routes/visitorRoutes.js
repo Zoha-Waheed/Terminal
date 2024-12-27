335d525c7117786e77970344d5c715cdfc20bb06
Venellopie <xuhaawaheed@gmail.com>
2024-12-27 16:21:07 +0500

    Visitors routes added

--- Routes/visitorRoutes.js
+++ Routes/visitorRoutes.js
@@ -7,6 +7,8 @@ const {
   updateVisitor,
   deleteVisitor,
 } = require("../Controllers/visitorController.js");
+const Visitor = require("../Models/visitorModel.js");
+const Review = require("../Models/reviewModel.js");
 
 router.route("/")
   .post(createVisitor)
@@ -16,5 +18,34 @@ router.route("/:id")
   .get(getVisitor)
   .put(updateVisitor)
   .delete(deleteVisitor);
+
+// Add a new route for visitors' activity (count of attractions reviewed)
+router.route("/activity")
+  .get(async (req, res) => {
+    try {
+      // Aggregate visitor data and count the number of reviews they have done
+      const visitorsWithActivity = await Visitor.aggregate([
+        {
+          $lookup: {
+            from: "reviews", // Reference to the 'reviews' collection
+            localField: "_id",
+            foreignField: "visitor",
+            as: "reviews",
+          },
+        },
+        {
+          $project: {
+            name: 1, // Include visitor name
+            email: 1, // Include visitor email
+            reviewsCount: { $size: "$reviews" }, // Count the number of reviews
+          },
+        },
+      ]);
+
+      res.status(200).json(visitorsWithActivity);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
 
 module.exports = router;
